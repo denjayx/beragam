20b15303547939a1450cec352db19280f8be238a
denjayx <denyw602@gmail.com>
2023-07-03 23:46:55 +0700

    feat:read and create budaya

--- app/Models/Budaya.php
+++ app/Models/Budaya.php
@@ -9,10 +9,14 @@ class Budaya extends Model
 {
     use HasFactory;
     protected $table = 'budaya';
-    protected $fillable = ['nama', 'foroUrl', 'deskripsi', 'provID', 'katID'];
+    protected $fillable = ['nama', 'foto_url', 'deskripsi', 'prov_id', 'kat_id'];
 
     public function provinsi()
     {
-        return $this->belongsTo(Provinsi::class);
+        return $this->belongsTo(Provinsi::class, 'prov_id', 'id');
+    }
+    public function kategori()
+    {
+        return $this->belongsTo(Kategori::class, 'kat_id', 'id');
     }
 }
