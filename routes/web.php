3e9adaa6b71f0459623eeeccb6388b9ef7dffe5e
denjayx <denyw602@gmail.com>
2023-07-03 23:50:40 +0700

    feat:add resource route budaya, kategori, provinsi

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Models\Budaya;
+use App\Http\Controllers\BudayaController;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\ProvinsiController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,3 +21,11 @@ Route::get('/', function () {
     $title = "Home";
     return view('home', compact('title'));
 });
+Route::get('/admin', function () {
+    $title = "Dashboard";
+    $budayas = Budaya::All();
+    return view('/admin/index', compact('title', 'budayas'));
+});
+Route::resource('/admin/budaya', BudayaController::class);
+Route::resource('/admin/provinsi', ProvinsiController::class);
+Route::resource('/admin/kategori', KategoriController::class);
