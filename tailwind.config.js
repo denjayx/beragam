678025dff513147a74aa16da27b5a6521d5e1d33
denjayx <denyw602@gmail.com>
2023-07-06 15:58:30 +0700

    feat:flowbite install

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ export default {
         "./node_modules/flowbite/**/*.js",
     ],
     theme: {
+        container: { center: true },
         extend: {},
     },
     plugins: [require("flowbite/plugin")],
