e26af671337ffecaa3c95f9423761dfc7301fe55
denjayx <denyw602@gmail.com>
2023-07-06 15:57:58 +0700

    feat:add migration and controller

---
+++ database/migrations/2023_07_05_130136_create_cultures_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cultures', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul');
+            $table->text('foto_url');
+            $table->text('deskripsi');
+            $table->foreignId('prov_id')->references('id')->on('provinces');
+            $table->foreignId('cat_id')->references('id')->on('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cultures');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProvinceController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('admin/', function () {
+    return view('admin/index');
+});
+Route::resource('admin/province/', ProvinceController::class);
